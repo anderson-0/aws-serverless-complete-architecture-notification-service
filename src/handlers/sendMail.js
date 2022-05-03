b9c5c5d53346e82b82a85f11109edc76785c4f61
Anderson <60013836+anderson-0@users.noreply.github.com>
2022-05-02 20:11:51 -0400

    feat: AWS SES test email configured

--- src/handlers/sendMail.js
+++ src/handlers/sendMail.js
@@ -1,9 +1,36 @@
+import AWS from 'aws-sdk';
+
+const ses = new AWS.SES({
+  apiVersion: '2010-12-01',
+  region: 'us-east-1',
+});
+
 async function sendMail(event, context) {
-  console.log(event);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello' }),
+  const params = {
+    Source: '<senderemail>', // SES SENDER
+    Destination: {
+      ToAddresses: ['<targetemail>'], // SES RECIPIENT
+    },
+    Message: {
+      Body: {
+        Text: {
+          Charset: "UTF-8",
+          Data: 'Hi there!'
+        }
+      },
+      Subject: {
+        Charset: "UTF-8",
+        Data: 'Test email',
+      },
+    },
   };
+
+  try {
+    const data = await ses.sendEmail(params).promise();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export const handler = sendMail;
